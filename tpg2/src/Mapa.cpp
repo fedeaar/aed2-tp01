1ab7941fc33fe180bfa5991e6eb3c49ace0694d1
Manuel Lakowsky <mlakowsky@dc.uba.ar>
2022-06-06 19:17:27 -0300

    Funciones Mapa

--- tpg2/src/Mapa.cpp
+++ tpg2/src/Mapa.cpp
@@ -1,19 +1,42 @@
 #include "Mapa.h"
+#include "Tipos.h"
 
 
-Mapa::Mapa(std::set<Nat> hs, std::set<Nat> vs) {}
+Mapa::Mapa(std::set<Nat> hs, std::set<Nat> vs) : _hs(hs), _vs(vs) {}
 
 
 Mapa::~Mapa() {}
 
 
-const std::set<Nat>& Mapa::horizontales() {}
+const std::set<Nat>& Mapa::horizontales() const{
+    return _hs;
+}
 
 
-const std::set<Nat>& Mapa::verticales() {}
+const std::set<Nat>& Mapa::verticales() const{
+    return _vs;
+}
 
 
-bool Mapa::esRio(const Pos& p) {}
+bool Mapa::esRio(const Pos& p) {
+    return _hs.count(p.y) == 1 || _vs.count(p.x) == 1;
+}
 
 
-Mapa Mapa::operator+(const Mapa& b) {}
+Mapa Mapa::operator+(const Mapa& b) {
+    Mapa res(_hs, _vs);
+
+    auto it = b.horizontales().begin();
+    while (it != b.horizontales().end()) {
+        res._hs.insert(*it);
+        it++;
+    }
+
+    it = b.verticales().begin();
+    while (it != b.verticales().end()) {
+        res._vs.insert(*it);
+        it++;
+    }
+
+    return res;
+}
--- tpg2/src/Mapa.h
+++ tpg2/src/Mapa.h
@@ -8,15 +8,15 @@ public:
     Mapa(std::set<Nat> hs, std::set<Nat> vs);
     ~Mapa();
 
-    const std::set<Nat>& horizontales();
-    const std::set<Nat>& verticales();
+    const std::set<Nat>& horizontales() const;
+    const std::set<Nat>& verticales() const;
 
     bool esRio(const Pos& p);
     Mapa operator+(const Mapa& b);
 
 private:
-    std::set<Nat> hs;
-    std::set<Nat> vs;
+    std::set<Nat> _hs;
+    std::set<Nat> _vs;
 };
 
 #endif //TP_SIMCITY_MAPA_H
